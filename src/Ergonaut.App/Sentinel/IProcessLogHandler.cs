f54b6012d06e04a0b4809f66fb7d03335a21ffac
Lukas Leindals <63400156+LukasLeindals@users.noreply.github.com>
2025-10-25 10:17:24 +0200

    remove outdated code

--- src/Ergonaut.App/Sentinel/IProcessLogHandler.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics;
-
-namespace Ergonaut.App.Sentinel;
-
-public interface IProcessLogHandler
-{
-    void HandleOutputData(object? sender, DataReceivedEventArgs e);
-    void HandleErrorData(object? sender, DataReceivedEventArgs e);
-}
